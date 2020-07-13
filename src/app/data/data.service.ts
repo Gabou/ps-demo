b4a7d6d26b874f4169662e34a742678427d8a186
Gabou <supergabou+github@outlook.fr>
2020-07-13 10:41:56 +0200

    Retrieve subscriptionType dynamically

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { UserSettings } from './user-settings';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -9,6 +9,10 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
 
   constructor(private http: HttpClient) { }
+
+  getSubscriptionTypes(): Observable<string[]> {
+    return of(['Monthly','Annual','Lifetime']);
+  }
 
   postUserSettingsForm(userSettings: UserSettings): Observable<any> {
       return this.http.post('https://putsreq.com/L6aN3vdJk0GL1Gh1A0gS', userSettings);
