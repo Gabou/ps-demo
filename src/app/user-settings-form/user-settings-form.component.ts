7f4dafe2380d5a5d30a50e05cbf6fd7f26ee433e
Gabou <supergabou+github@outlook.fr>
2020-07-13 11:13:55 +0200

    replace radiobutton form with these of ngx-bootstrap

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -41,16 +41,16 @@ export class UserSettingsFormComponent implements OnInit {
   }
 
   onSubmit(form : NgForm) {
-    console.log('in onSubmit: ', form.valid);
-    if (form.valid) {
-      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
-        result => console.log('success: ', result),
-        error => this.onHttpError(error)
-      );
-    } else {
-      this.postError = true;
-      this.postErrorMessage = "Please fix the above errors";
-    }
-  }
+    console.log('in onSubmit: ', form.value);
+  //   if (form.valid) {
+  //     this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+  //       result => console.log('success: ', result),
+  //       error => this.onHttpError(error)
+  //     );
+  //   } else {
+  //     this.postError = true;
+  //     this.postErrorMessage = "Please fix the above errors";
+  //   }
+   }
 
 }
